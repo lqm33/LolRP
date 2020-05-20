3a82ef3955b867d0016557a8c7c4f5fdc9872110
lqm33 <51096589+lqm33@users.noreply.github.com>
2020-05-20 10:27:14 +0300

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,57 @@
+import pyautogui as r
+
+#lqm33.py
+
+import random
+print(""""
+
+██╗      ██████╗ ███╗   ███╗██████╗ ██████╗ 
+██║     ██╔═══██╗████╗ ████║╚════██╗╚════██╗
+██║     ██║   ██║██╔████╔██║ █████╔╝ █████╔╝
+██║     ██║▄▄ ██║██║╚██╔╝██║ ╚═══██╗ ╚═══██╗
+███████╗╚██████╔╝██║ ╚═╝ ██║██████╔╝██████╔╝
+╚══════╝ ╚══▀▀═╝ ╚═╝     ╚═╝╚═════╝ ╚═════╝ 
+
+Instagram:lqm33.py                                                                                              
+
+""")
+
+
+liste=["Q","W","E","R","Y","U","I","O","P","A","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0"]
+
+
+
+while True:
+    a=random.choice(liste or liste)
+    b=random.choice(liste or liste)
+    c=random.choice(liste or liste)
+    d=random.choice(liste or liste)
+    e=random.choice(liste or liste)
+    f=random.choice(liste or liste)
+    h=random.choice(liste or liste)
+    l=random.choice(liste or liste)
+    m=random.choice(liste or liste)
+    n=random.choice(liste or liste)
+    o=random.choice(liste or liste)
+    p=random.choice(liste or liste)
+    ra=random.choice(liste or liste)
+    s=random.choice(liste or liste)
+    t=random.choice(liste or liste)
+    u=random.choice(liste or liste)
+    z=a+b+c+d+e+f+h+l+m+n+o+p+ra+s+t+u
+    r1=("REP-TR-400-"+z)
+    r2=("REP-TR-840-"+z)
+    r3=("REP-TR-1780-"+z)
+    r4=("REP-TR-3620-"+z)
+    r5=("REP-TR-6450-"+z)
+    r6=("REP-TR-12800-"+z)
+    with open ("kodlar.txt","a+") as rpkodları:
+        rpkodları.write(r1+"\n")
+        rpkodları.write(r2+"\n")
+        rpkodları.write(r3+"\n")
+        rpkodları.write(r4+"\n")
+        rpkodları.write(r5+"\n")
+        rpkodları.write(r6+"\n")
+        ####
+        
+    
